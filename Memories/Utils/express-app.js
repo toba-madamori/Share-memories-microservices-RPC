ac66774698b68577830276c00df38cc258d8170d
Toba <tobajamezmadamori@gmail.com>
2022-11-20 16:35:01 +0100

    updated application instance

--- Memories/Utils/express-app.js
+++ Memories/Utils/express-app.js
@@ -6,7 +6,6 @@ require('express-async-errors')
 const express = require('express')
 const app = express()
 const memoryApi = require('../Api/Endpoints/memories')
-const appEvents = require('../Api/Endpoints/app-events')
 
 // extra security packages
 const cors = require('cors')
@@ -35,9 +34,6 @@ app.get('/', (req, res) => {
 })
 
 // routes
-
-// listen to events
-appEvents(app)
 
 // memory api
 memoryApi(app)
